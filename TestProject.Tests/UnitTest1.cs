6f6adba4e94833e4a7652ebc789fb0cbbb246ce7
unknown <sporna.artur@gmail.com>
2018-12-15 14:19:36 +0100

    feature_1 finish

--- TestProject.Tests/UnitTest1.cs
+++ TestProject.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentAssertions;
 using Xunit;
 
 namespace TestProject.Tests
@@ -8,11 +9,21 @@ namespace TestProject.Tests
         [Fact]
         public void return_0__when__set_initial_data_to_0_and_expression_contains_only_init_data()
         {
-            var testCalculator = new Calculator();
-            
-            testCalculator.AddNumbers(5, 10);
+            var calculatorValue = new Calculator(0);
 
+            calculatorValue.Value.Should().Be(0);
+        }
+
+        [Fact]
+        public void default_set_to_0__when__init_without_initial_data()
+        {
+            var calculatorValue = new Calculator();
+
+            calculatorValue.Value.Should().NotBe(1);
+            calculatorValue.Value.Should().Be(0);
             
         }
+        
+
     }
 }
