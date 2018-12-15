b945c3b188c05b7af025c7223d2d05d39de6fb2b
Krzysztof Gieroń <gieron.krzysztof@gmail.com>
2018-12-15 13:51:14 +0100

    add failing tesst

--- TestProject.Tests/UnitTest1.cs
+++ TestProject.Tests/UnitTest1.cs
@@ -6,9 +6,13 @@ namespace TestProject.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void return_0__when__set_initial_data_to_0_and_expression_contains_only_init_data()
         {
+            var testCalculator = new Calculator();
+            
+            testCalculator.AddNumbers(5, 10);
 
+            
         }
     }
 }
--- TestProject/Class1.cs
+++ TestProject/Class1.cs
@@ -5,4 +5,11 @@ namespace TestProject
     public class Class1
     {
     }
+
+    public class Calculator
+    {
+        public void AddNumbers(int a, int b){
+            throw new Exception();
+        }        
+    }
 }
